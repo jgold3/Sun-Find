c1aeb447238555c22d856efc3162e716349ef36d
jgold3 <32280330+jgold3@users.noreply.github.com>
2021-06-01 13:18:55 -0400

    Set up skeleton structure for the map screen

--- expo-client/pages/map.js
+++ expo-client/pages/map.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {Component} from 'react';
 import { Dimensions, StyleSheet, Text, View } from 'react-native';
 import MapView from 'react-native-maps';
 
@@ -15,11 +15,17 @@ const styles = StyleSheet.create({
     },
 });
 
-export default MapScreen = () => {
-    return (
+export default class MapScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            locations: [],
+        };
+    }
+
+    render = () => (
         <View style={styles.container}>
             <MapView style={styles.map} />
         </View>
     );
-    
 };
