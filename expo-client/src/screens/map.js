664c4834b657720be66b4753bef386eeec71f53a
jgold3 <32280330+jgold3@users.noreply.github.com>
2021-08-05 11:34:57 -0400

    Update map.js

--- expo-client/src/screens/map.js
+++ expo-client/src/screens/map.js
@@ -1,7 +1,7 @@
 import React, { useCallback, useState } from 'react';
-import { ActivityIndicator, View } from 'react-native';
+import { ActivityIndicator, Image, Dimensions, View, Text } from 'react-native';
 import { useFocusEffect } from '@react-navigation/native';
-import { StatusBar } from 'expo-status-bar';
+import Carousel from 'react-native-snap-carousel';
 import * as Location from 'expo-location';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 import SunriseMarker from '../components/SunriseMarker';
@@ -15,9 +15,9 @@ export default MapScreen = () => {
   const [region, setRegion] = useState(null);
 
   const [coordinates, setCoordinates] = useState([
-    {name: '1', latitude: 39.470912, longitude: -75.888454},
-    {name: '2', latitude: 39.474624, longitude: -75.878442},
-    {name: '3', latitude: 39.466430, longitude: -75.877609},
+    {name: '1', latitude: 39.470912, longitude: -75.888454, images: [require('../../assets/logo.png')]},
+    {name: '2', latitude: 39.474624, longitude: -75.878442, images: [require('../../assets/logo.png')]},
+    {name: '3', latitude: 39.466430, longitude: -75.877609, images: [require('../../assets/logo.png')]},
   ]);
 
   getLocation = async () => {
@@ -37,6 +37,12 @@ export default MapScreen = () => {
   
   };
 
+  const renderCarouselItem = ({ item, index }) => {
+    <View>
+      <Text>{item.name}</Text>
+      <Image source={item.images[0]}/>
+    </View>
+  };
 
   useFocusEffect(
     useCallback(() => {
@@ -49,31 +55,84 @@ export default MapScreen = () => {
     }, [])
   );
 
-  return (
-    <View style={styles.container}>
-      {
-        errMsg ? <Text>{errMsg}</Text> :
-          havePermission && (location === null || region === null) ? ( <ActivityIndicator size="large" color="#ff4500" /> ) : (
-            <MapView 
-              style={styles.map}
-              provider={PROVIDER_GOOGLE}
-              region={region}
-            > 
-              {location ? <UserMarker location={location} /> : null}
-              {
-                coordinates.map(coord => (
-                  <SunriseMarker
-                    key={coord.name}
-                    name={coord.name}
-                    coordinate={{latitude: coord.latitude, longitude: coord.longitude}}
-                    image={require('../../assets/logo.png')}
-                  />
-                ))
-              }
-            </MapView>
-          )
-      }
-      <StatusBar style="dark" />
-    </View>
-  );
+  if (errMsg) {
+    return (
+      <View style={styles.container}>
+        <Text>{errMsg}</Text>
+      </View>
+    );
+  } 
+  else if (havePermission && (location === null || region === null)) {
+    console.log('Here boi')
+    return (
+      <ActivityIndicator size="large" color="#ff4500" />
+    );
+  }
+  else if (!havePermission) {
+    return (
+      <View style={styles.container}>
+        <Text>You need to accept request for location, if you want to post points</Text>
+      </View>
+    );
+  }
+  else {
+    return (
+      <View style={styles.container}>
+        <MapView 
+          style={styles.map}
+          provider={PROVIDER_GOOGLE}
+          region={region}
+        > 
+          {location ? <UserMarker location={location} /> : null}
+          {
+            coordinates.map(coord => (
+              <SunriseMarker
+                key={coord.name}
+                name={coord.name}
+                coordinate={{latitude: coord.latitude, longitude: coord.longitude}}
+                image={coord.images[0]}
+              />
+            ))
+          }
+        </MapView>
+        <Carousel
+            ref={(c) => { carousel = c; }}
+            data={coordinates}
+            renderItem={renderCarouselItem}
+            sliderWidth={Dimensions.get('window').width}
+            itemWidth={200}
+        />
+      </View>
+    );
+  }
+  
+
+  // return (
+  //   <View style={styles.container}>
+  //     {
+  //       errMsg ? <Text>{errMsg}</Text> :
+  //         havePermission && (location === null || region === null) ? ( <ActivityIndicator size="large" color="#ff4500" /> ) : (
+            // <MapView 
+            //   style={styles.map}
+            //   provider={PROVIDER_GOOGLE}
+            //   region={region}
+            // > 
+            //   {location ? <UserMarker location={location} /> : null}
+            //   {
+            //     coordinates.map(coord => (
+            //       <SunriseMarker
+            //         key={coord.name}
+            //         name={coord.name}
+            //         coordinate={{latitude: coord.latitude, longitude: coord.longitude}}
+            //         image={coord.images[0]}
+            //       />
+            //     ))
+            //   }
+            // </MapView>
+            
+  //         )
+  //     }
+      
+  //   </View>
+  // );
 };
